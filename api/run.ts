904cbf7393529c1f0774b8eaefc821da2b629492
Paul Kinlan <paul.kinlan@gmail.com>
2023-08-11 21:17:30 +0000

    dirname

--- api/run.ts
+++ api/run.ts
@@ -2,7 +2,11 @@ import { Board } from "@google-labs/breadboard";
 
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const { q } = req.query;
