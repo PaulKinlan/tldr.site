72eaae0989839cca4d3680be54eb2fefa806dfe7
Paul Kinlan <paul.kinlan@gmail.com>
2023-08-12 20:40:54 +0000

    Adding open props

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { Home } from './routes/home.js';
 import { Header } from './components/header.js';
 import { Footer } from './components/footer.js';
 
+import "./css/index.css";
+
 const Container = ({ children }) => {
   return (<>
     <Header></Header>
